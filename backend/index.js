bb4243090384905029287a913973896cd0fc1be4
re <121850050+reized@users.noreply.github.com>
2025-05-25 21:07:41 +0700

    add patch method

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const corsOptions = {
         "http://localhost:5173",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 
