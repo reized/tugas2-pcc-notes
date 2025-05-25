2cefbdee560f40ca96d2ec224b8d4aea9df8947b
re <121850050+reized@users.noreply.github.com>
2025-05-25 20:31:03 +0700

    update url fe

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+    origin: [
+        "https://frontend-refaiza-dot-f-02-450706.uc.r.appspot.com",
+        "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
